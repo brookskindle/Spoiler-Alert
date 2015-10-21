f9219b871fcebc625925bb297e5a0a8b9efa5d6c
Brooks Kindle <brooks.kindle@wsu.edu>
2015-10-20 21:32:43 -0700

    Moved code back to spoiler.py, added pylint and flake8 to requirements.

--- spoiler.py
+++ spoiler.py
@@ -11,20 +11,95 @@ import random
 
 # Third party imports.
 from flask import Flask
+from flask import flash  # Flash a message on the screen.
+from flask import redirect  # To redirect to another page
 from flask import render_template  # For html template rendering.
 from flask import request  # For obtaining POST information (?)
-from flask import redirect  # To redirect to another page
+from flask import url_for  # Gets the url for a given name
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 # Local imports.
-from database import db
-from models.models import Post, User
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db = SQLAlchemy(app)  # Create the db
+login_manager = LoginManager()
+login_manager.init_app(app)  # Create the login_manager
+
+"""-------------------------------------------------------------------------"""
+"""---------------------------------MODELS----------------------------------"""
+"""-------------------------------------------------------------------------"""
+
+
+class User(db.Model):
+    # Since we're using Flask-Login, User must be created a specific way:
+    # See: https://flask-login.readthedocs.org/en/latest/#your-user-class
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(160), unique=True)
+    password = db.Column(db.String(64), unique=False)
+    authenticated = db.Column(db.Boolean, default=False)
+    posts = db.relationship("Post", backref="user", lazy="dynamic")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return "<user: {}>".format(self.username)
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return True  # All users are active
+
+    def is_anonymous(self):
+        return False  # Disable anonymous users
+
+    def get_id(self):
+        return self.id
+
+
+class Post(db.Model):
+    """Represents a post made by a user."""
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(160), unique=False)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __init__(self, userid, content=None):
+        self.content = content
+        # TODO: validate user id?
+        self.userid = userid
+
+    def __repr__(self):
+        return "id={0}, content={1}".format(self.id, self.content)
+
+
+def init_db():
+    """Initializes the database."""
+    db.create_all()
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    """
+    Return the User object from a given user_id
+    """
+    return User.query.get(user_id)
+
+"""-------------------------------------------------------------------------"""
+"""---------------------------------ROUTES----------------------------------"""
+"""-------------------------------------------------------------------------"""
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     """Remove database session at end of request/when app is shut down."""
-    db_session.remove()
+    db.session.remove()
+
 
 @app.route("/")
 def index():
@@ -38,9 +113,11 @@ def index():
         spoiler = posts[i].content
     return render_template("index.html", content=spoiler)
 
+
 @app.route("/submit/")
 def submit():
     return render_template("submit.html")
+
 
 @app.route("/submit/", methods=["POST"])
 def submit_post():
@@ -48,11 +125,12 @@ def submit_post():
     spoiler = request.form['text']
     # TODO: Does this need to go through any user sanitization?
     post = Post(spoiler)
-    db_session.add(post)
-    db_session.commit()
+    db.session.add(post)
+    db.session.commit()
     return redirect("/", code=302)
 
-@app.route("/register/", methods=["GET","POST"])
+
+@app.route("/register/", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
@@ -61,10 +139,11 @@ def register():
     # TODO: do we need to check for username validity?
     db.session.add(user)
     db.session.commit()
-    falsh("Successfully registered.")
+    flash("Successfully registered.")
     return redirect(url_for("login"))
 
-@app.route("/login/", methods=["GET","POST"])
+
+@app.route("/login/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
@@ -72,10 +151,11 @@ def login():
     # TODO: do we need to check login credentials?
     return redirect(url_for("index"))
 
+
 @app.route("/logout/")
 def logout():
     """Logs a user out, if he was logged in."""
-    pass
+    return "TODO"
 
 if __name__ == "__main__":
     init_db()
