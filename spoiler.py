522c1fd73f2308a4f5ba75f340898d332631ec4f
Brooks Kindle <brooks.kindle@wsu.edu>
2015-10-30 23:21:48 -0700

    Added WTForms for register, login, and submit pages.

--- spoiler.py
+++ spoiler.py
@@ -21,6 +21,9 @@ from flask.ext.login import LoginManager, login_required, login_user
 from flask.ext.login import UserMixin
 from flask.ext.login import current_user, logout_user
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from wtforms import StringField, TextField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Local imports.
@@ -33,9 +36,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)  # Create the login_manager
 bootstrap = Bootstrap(app)
 
-"""-------------------------------------------------------------------------"""
-"""---------------------------------MODELS----------------------------------"""
-"""-------------------------------------------------------------------------"""
+# --------------------------------------------------------------------------
+# ----------------------------------MODELS----------------------------------
+# --------------------------------------------------------------------------
 
 
 class User(UserMixin, db.Model):
@@ -86,9 +89,42 @@ def user_loader(user_id):
     """
     return User.query.get(int(user_id))
 
-"""-------------------------------------------------------------------------"""
-"""---------------------------------ROUTES----------------------------------"""
-"""-------------------------------------------------------------------------"""
+# --------------------------------------------------------------------------
+# ----------------------------------FORMS-----------------------------------
+# --------------------------------------------------------------------------
+
+
+class PostForm(Form):
+    """
+    A form for submitting a spoiler post.
+    """
+    post = TextField("Give us a spoiler!", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class LoginForm(Form):
+    """
+    A form to allow users to log in.
+    """
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
+
+
+class RegisterForm(Form):
+    """
+    A form that people can use to register an account.
+    """
+    username = StringField("Create a username", validators=[DataRequired()])
+    password = PasswordField("Create a password", validators=[DataRequired(),
+                EqualTo('password_repeat', message="Passwords must match")])
+    password_repeat = PasswordField("Repeat password",
+                                    validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+# --------------------------------------------------------------------------
+# ----------------------------------ROUTES----------------------------------
+# --------------------------------------------------------------------------
 
 
 @app.teardown_appcontext
@@ -114,54 +150,56 @@ def index():
 @login_required
 def submit():
     """Adds a submitted spoiler."""
-    if request.method == "GET":
-        return render_template("submit.html")
-    # Else POST
-    user_id = current_user.get_id()
-    spoiler = request.form['text']
-    post = Post(user_id, spoiler)
-    db.session.add(post)
-    db.session.commit()
-    return redirect("/", code=302)
+    content = None
+    user_id = None
+    form = PostForm()
+    if form.validate_on_submit():
+        user_id = current_user.get_id()
+        content = form.post.data
+        post = Post(user_id, content)
+        db.session.add(post)
+        db.session.commit()
+        return redirect("/", code=302)
+    return render_template("submit.html", form=form)
 
 
 @app.route("/register/", methods=["GET", "POST"])
 def register():
     # TODO: implement https for a production server
-    if request.method == "GET":
-        return render_template("register.html")
-    # Else "POST"
-    username = request.form["username"]
-    password = request.form["password"]
-    user = User.query.filter_by(username=username).first()
-    if user is not None:  # User already exists
-        flash("{} is already in use.".format(username))
-        return render_template("register.html")
-    # User doesn't exist, add him!
-    user = User()
-    user.username = username
-    user.password = password
-    db.session.add(user)
-    db.session.commit()
-    flash("Successfully registered.")
-    return redirect(url_for("login"))
+    form = RegisterForm()
+    if form.validate_on_submit():  # POST and form data validated.
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user is not None:  # User already exists
+            flash("{} is already in use.".format(username))
+        else:  # User doesn't exist, add him!
+            user = User()
+            user.username = username
+            user.password = password
+            db.session.add(user)
+            db.session.commit()
+            flash("Successfully registered.")
+            return redirect(url_for("login"))
+    return render_template("register.html", form=form)
 
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     # TODO: implement https for a production server
-    if request.method == "GET":
-        return render_template("login.html")
-    # Else "POST"
-    username = request.form["username"]
-    password = request.form["password"]
-    user = User.query.filter_by(username=username).first()
-    if user is not None and user.verify_password(password):
-        # Valid username and password
-        login_user(user)
-        return redirect(url_for("submit"))
-    flash("Error, incorrect username and/or password")
-    return redirect(url_for("login"))
+    form = LoginForm()
+    if form.validate_on_submit():  # POST request with valid form data.
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.verify_password(password):
+            # Valid credentials supplied.
+            login_user(user)
+            return redirect(url_for("submit"))
+        else:
+            # Invalid login credentials.
+            flash("Invalid username or password supplied.")
+    return render_template("login.html", form=form)
 
 
 @app.route("/logout/", methods=["GET"])
