ca716f066903856d16776e6b8972c8640ead1948
Brooks Kindle <brooks.kindle@wsu.edu>
2015-12-11 11:30:39 -0800

    Proper login redirect with flask_login and added a few access control decorators.

--- spoiler.py
+++ spoiler.py
@@ -11,14 +11,14 @@ import random
 from functools import wraps
 
 # Third party imports.
-from flask import Flask
+from flask import Flask, abort
 from flask import flash  # Flash a message on the screen.
 from flask import redirect  # To redirect to another page
 from flask import render_template  # For html template rendering.
 from flask import request  # For obtaining POST information (?)
 from flask import url_for  # Gets the url for a given name
 from flask.ext.bootstrap import Bootstrap  # Pretty user interface
-from flask.ext.login import LoginManager, login_user
+from flask.ext.login import LoginManager, login_user, login_required
 from flask.ext.login import UserMixin
 from flask.ext.login import current_user, logout_user
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -36,6 +36,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 app.secret_key = "THIS ISN'T A VERY SECURE SECRET KEY!"
 db = SQLAlchemy(app)  # Create the db
 login_manager = LoginManager()
+login_manager.login_view = "login"
 login_manager.init_app(app)  # Create the login_manager
 bootstrap = Bootstrap(app)
 manager = Manager(app)
@@ -45,13 +46,38 @@ migrate = Migrate(app, db)
 # -----------------------------------------------------------------------------
 # ----------------------------------DECORATORS---------------------------------
 # -----------------------------------------------------------------------------
-def login_required(f):
+def disabled(f):
+    """
+    Decorator that returns an HTTP 503 (Service Unavailable) error code when a
+    route is invoked.
+
+    Because of the order of decorator execution, this decorator should come
+    immediatly after registering the route and before any other decorators.
+
+    As an example:
+        @app.route("/account/")
+        @disabled
+        @...(other decorators)
+        def account():
+            pass
+    """
     @wraps(f)
     def _wrapper(*args, **kwargs):
-        if current_user.is_anonymous:
-            return redirect(url_for("login", next_url=request.path))
-        return f(*args, **kwargs)
+        return abort(503)
     return _wrapper
+
+def user_allowed(user_name):
+    """ Allows only a given user access to a route.  """
+    def _decorator(f):
+        @wraps(f)
+        def _wrapper(*args, **kwargs):
+            if current_user.is_anonymous:
+                pass
+            elif current_user.username == user_name:
+                return f(*args, **kwargs)
+            return abort(403)
+        return _wrapper
+    return _decorator
 
 # --------------------------------------------------------------------------
 # ----------------------------------MODELS----------------------------------
@@ -212,7 +238,7 @@ def login():
         if user is not None and user.verify_password(password):
             # Valid credentials supplied.
             login_user(user)
-            return redirect(request.args.get("next_url") or url_for("index"))
+            return redirect(request.args.get("next") or url_for("index"))
         else:
             # Invalid login credentials.
             flash("Invalid username or password supplied.")
